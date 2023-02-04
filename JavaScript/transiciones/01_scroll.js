20fbd5331f3eb38dd98d103fa82e047d26fa0846
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-04 13:31:32 -0500

    Prueba de commit
    Esta es una prueba del repo

--- JavaScript/transiciones/01_scroll.js
+++ JavaScript/transiciones/01_scroll.js
@@ -1,3 +1,11 @@
+
+/**
+ * 
+ * SE UTILIZA PARA DESPLAZARSE AL TO
+ * 
+ * 
+ */
+
 export const moverAlTop = () => {
   const html:HTMLElement | null = document.getElementById('modal-component');
   html!.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center'});
