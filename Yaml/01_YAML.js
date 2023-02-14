e6f80b525727e9480351590eddb5fafbdf69ec77
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-13 21:46:22 -0500

    Introducción a YAML

---
+++ Yaml/01_YAML.js
@@ -0,0 +1,10 @@
+/*
+- Lenguaje de seralización de facil comprensión
+- Es muy usado en contenedores
+- Es muy parecido al JSON
+- Puede asociarse con XML
+
+USOS
+- Escritura de datos clave valor
+
+*/
