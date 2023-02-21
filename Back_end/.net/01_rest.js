510575a6516d5e1de96b411144f0517af9b75ae8
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-20 20:53:27 -0500

    Respuestas

--- Back_end/.net/01_rest.js
+++ Back_end/.net/01_rest.js
@@ -8,4 +8,23 @@
  * -- Simplicidad
  * -- Confiabilidad 
  * 
+ * 
+ * HTTP REST
+ * - GET
+ * - POST
+ * - PUT
+ * - PATCH
+ * - DELETE
+ * 
+ * MANEJO DE URL
+ * - Manejo de URl por cada recurso, por tal debe de ser una URL diciente
+ * 
+ * RESPUESTAS
+ * - 100-199 Informativas
+ * - 200-299 Sastisfactorias
+ * - 300-399 Redirecciones 
+ * - 400-499 Errores de clientes
+ * - 500-599 Errores de servidoress
+ * 
+ * 
 */
