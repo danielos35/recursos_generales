8505cf795cd64a28adb266972bb2d4545643ff52
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-20 21:03:39 -0500

    Modelo Controlador en .NET

---
+++ Back_end/.net/02_mvc.cs
@@ -0,0 +1,12 @@
+/*
+
+MVC 
+- Patron que nos permite separa responsabilidades
+- Nos permite crear MODELOS para el guardado de los datos
+- El CONTROLADOR se va encargar de maneja toda la logica necesaria
+- VIEW, interface de usuario
+
+IMPORTANTE
+En las API al no tener VIEW es que se utiliza un Modelo MC (modelo y controladores)
+
+*/ 
