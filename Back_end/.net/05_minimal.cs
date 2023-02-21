50d6d2c4f8a6d4d883d77101975568847c846602
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-21 11:02:33 -0500

    Middlelwares

---
+++ Back_end/.net/05_minimal.cs
@@ -0,0 +1,7 @@
+/*
+
+MINIMAL API
+- Esta diseñado para proyectos simples que no tengan muchos endpoints. Por ejemplo para hacer un CRUD, para proyectos mas grandes es recomendable usar WEBAPI
+- NO ESTA DISEÑADO PARA PROYECTOS GRANDES
+- Esta diseñado para cosas muy minimas, proyectos pequeños
+*/
---
+++ Back_end/.net/06_meddlewares.cs
@@ -0,0 +1,27 @@
+/*
+
+Middleware
+- Instrucciones de codigo que se realizan al realizar una petición HTTP
+- Provee ejecución a través de capas
+- Facilita la implementación de interceptores y filtros sobre las peticiones de un API
+
+Ciclo de vida de un request
+- Camina que recorre una petición a través de los middelware
+
+Orden de Middleware en .NET
+- Request
+- ExceptionHandler
+- HSTS
+- HttpRedirection
+- Static Files
+- Routing
+- CORS
+- Authentication
+- Authorization 
+- Custom middlewares
+- END-POINT
+
+
+
+
+*/
