1cf2d04ff6d099a72683661f38aa12daa0bd3301
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-20 20:50:13 -0500

    Terminos REST

---
+++ Back_end/.net/01_rest.js
@@ -0,0 +1,11 @@
+/**
+ * REST
+ * - Estilo de arquitectura para el diseño de interfaces web
+ * - Permite el manejo de recursos desde el lado del servidor
+ * - Contiene
+ * -- Rendimiento
+ * -- Escalabilidad
+ * -- Simplicidad
+ * -- Confiabilidad 
+ * 
+*/
