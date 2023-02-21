f0953bdb9ad832dff954af801696d6ad18301a43
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-20 21:10:47 -0500

    Analisis del proyecto

---
+++ Back_end/.net/03_analisis_API.cs
@@ -0,0 +1,19 @@
+/*
+CONTROLADORES
+- Es importante agregar la palabra controller en el nombre de las clases de los controladores por ejemplo InfoController
+
+WEPAPI.CSPROJ
+- Se utiliza para conocer los datos del proyecto
+
+SDK
+- Contiene un conjunto de librerias base para realizar ciertas tareas en este caso el API
+
+APP SETTINGS
+- Configuración tipo servicios
+
+LAUNCH SETTINGS
+- Configuración de la manera en la que se va ejecutar el aplicativo
+
+
+
+*/ 
