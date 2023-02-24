5d96ec3126c6af368ba1dbb7e11484ca757274b5
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-23 21:40:12 -0500

    Loggins en .NET

---
+++ Back_end/.net/07_Inyeccion_dependencias.cs
@@ -0,0 +1,25 @@
+/*
+AddScope
+- Implementa siempre la misma instancia
+
+AddSingleton
+- Se crea una única implementación | No es recomendado, pues la dependencia se crea en memoria
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ Back_end/.net/08_loggin.cs
@@ -0,0 +1,7 @@
+/*
+Trazabilidad en API
+
+- Se utiliza para escribir mensajes de seguimiento
+- Hay varios tipos de loggins que podemos usar en nuestra aplicación
+
+*/
