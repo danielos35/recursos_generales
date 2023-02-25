8937d9639beb10719c21bf212527c6c5f1156eda
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-24 20:21:14 -0500

    Creación servicios

--- Back_end/.net/08_nuget.cs
+++ Back_end/.net/08_nuget.cs
@@ -1,9 +1,9 @@
 /*
 
 Nuget
-- Es la plataforma para instalar librerias de C#
-- Es similar npm install
-- Para instalar paquetes directamente desde VS vamos a la ruta Project>ManagetPackage
+- Es la plataforma para instalar librerias de C#.
+- Es similar npm install.
+- Para instalar paquetes directamente desde VS vamos a la ruta Project>ManagetPackage.
 
 
 
---
+++ Back_end/.net/09_servicios.cs
@@ -0,0 +1,21 @@
+/*
+Servicios:
+
+- En un API, los servicios Normalmente son aquellos que tienen toda la lógica para poder comunicarnos con la base de datos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
