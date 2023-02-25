351496439823fec70b525298f7f0b4d237493ea5
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-02-24 20:09:32 -0500

    paquetes de .net

---
+++ Back_end/.net/08_nuget.cs
@@ -0,0 +1,20 @@
+/*
+
+Nuget
+- Es la plataforma para instalar librerias de C#
+- Es similar npm install
+- Para instalar paquetes directamente desde VS vamos a la ruta Project>ManagetPackage
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
