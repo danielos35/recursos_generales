ca1abe04293594a046dfcb46b3ea6b98163628fc
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-03-20 08:53:36 -0500

    Capturar valor en un selector #2

--- Angular/captura_de_datos_selectores.ts
+++ Angular/captura_de_datos_selectores.ts
@@ -1,6 +1,5 @@
-/**
- * 
- * <div class="item_t">
+/*
+  <div class="item_t">
     <mat-form-field appearance="outline" style="transform: translateY(10px) scale(0.9);">
       <mat-select [disabled]="guardando" [(ngModel)]="cliente.cliente_estado_id" (valueChange)="guardarDatosCliente($event, 'estado')" >
         <mat-option *ngFor="let item of estados || []" [value]="item.id" style="font-size: 0.7rem;">
@@ -9,13 +8,10 @@
       </mat-select>
     </mat-form-field>
   </div>
- * 
- * */ 
 
 
 /* 
 TS
-
 
   guardarDatosCliente(data:any, tipoDeDato:any){
     if(tipoDeDato === 'estado') // --> asignar datos capturados, pues por defecto no asigna el valor seleccionado
